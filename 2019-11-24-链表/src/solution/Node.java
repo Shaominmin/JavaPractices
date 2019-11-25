ede5c727815dd4fdfcfd70214612461d38885e94
Shaomin <2583456081@qq.com>
2019-11-25 19:40:31 +0800

    java 链表

---
+++ 2019-11-24-链表/src/solution/Node.java
@@ -0,0 +1,10 @@
+package solution;
+
+public class Node {
+    int val;
+    Node next = null;
+
+    Node(int val) {
+        this.val = val;
+    }
+}
---
+++ 2019-11-24-链表/src/solution/RNode.java
@@ -0,0 +1,14 @@
+package solution;
+
+public class RNode {
+    int val;
+    RNode next = null;
+    RNode random = null;
+
+    RNode() {
+    }
+
+    RNode(int val) {
+        this.val = val;
+    }
+}
